46c98bc9997de1e82d7f889d207c8b9e7f5e1ff4
Mavludin Abdulmutalibov <mavludin5522@mail.ru>
2024-07-07 22:27:49 +0300

    fix

--- src/components/UI/MovieCartList/MovieCartList.tsx
+++ src/components/UI/MovieCartList/MovieCartList.tsx
@@ -1,22 +1,3 @@
-// import { observer } from "mobx-react-lite";
-// import styles from "./MovieCartList.module.css";
-// import { Movie } from "../../types/types";
-// import { MovieCart } from "../MovieCart/MovieCart";
-
-// type MovieCartListProps = {
-//   list: Movie[];
-// };
-
-// export const MovieCartList = observer(({ list }: MovieCartListProps) => {
-//   return (
-//     <div className={styles.container}>
-//       {list?.map((movie) => {
-//         return <MovieCart key={movie.id} data={movie} />;
-//       })}
-//     </div>
-//   );
-// });
-
 import { observer } from "mobx-react-lite";
 import styles from "./MovieCartList.module.css";
 import { Movie } from "../../../types/types";
